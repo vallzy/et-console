9eb6b7eb7d02abb59c31c7a8b77233be6d7e28bd
vallzy <45981588+vallzy@users.noreply.github.com>
2019-04-30 12:54:33 +0000

    te
    stest

--- ETConsolePinvoke/PreferenceWindow.Designer.cs
+++ ETConsolePinvoke/PreferenceWindow.Designer.cs
@@ -36,6 +36,7 @@
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.label2 = new System.Windows.Forms.Label();
             this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.button1 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.trkWait)).BeginInit();
             this.groupBox2.SuspendLayout();
@@ -85,10 +86,11 @@
             // 
             // groupBox2
             // 
+            this.groupBox2.Controls.Add(this.button1);
             this.groupBox2.Controls.Add(this.label2);
             this.groupBox2.Location = new System.Drawing.Point(12, 128);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(356, 169);
+            this.groupBox2.Size = new System.Drawing.Size(356, 129);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Visual";
@@ -105,6 +107,15 @@
             // errorProvider1
             // 
             this.errorProvider1.ContainerControl = this;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(106, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(61, 21);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // PreferenceWindow
             // 
@@ -138,5 +149,6 @@
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Button button1;
     }
 }
